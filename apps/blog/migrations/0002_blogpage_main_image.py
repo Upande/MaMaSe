b591fb88c819686ef0a92e74a8d0ef11e4713ff0
itsMagondu <MagonduNjenga@gmail.com>
2015-07-21 17:15:00 +0300

    Added nice demo page on a blog

---
+++ apps/blog/migrations/0002_blogpage_main_image.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('wagtailimages', '0006_add_verbose_names'),
+        ('blog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='blogpage',
+            name='main_image',
+            field=models.ForeignKey(related_name='+', on_delete=django.db.models.deletion.SET_NULL, blank=True, to='wagtailimages.Image', null=True),
+        ),
+    ]
