d73448490afc6f83a543eecd57bf738f16933cf8
itsMagondu <MagonduNjenga@gmail.com>
2015-07-22 16:28:55 +0300

    Added index page

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
 from wagtail.wagtailsearch import index
-from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailcore.models import Page, Orderable
+from wagtail.wagtailadmin.edit_handlers import (FieldPanel,
+                                                InlinePanel,
+                                                MultiFieldPanel,
+                                                PageChooserPanel)
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+
+from modelcluster.fields import ParentalKey
 
 class BlogPage(Page):
     main_image = models.ForeignKey(
@@ -30,9 +35,53 @@ class BlogPage(Page):
         FieldPanel('body', classname="full")
     ]
 
+
+class LinkFields(models.Model):
+    link_external = models.URLField("External link", blank=True)
+    link_page = models.ForeignKey(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        related_name='+'
+    )
+
+    @property
+    def link(self):
+        if self.link_page:
+            return self.link_page.url
+        else:
+            return self.link_external
+
+    panels = [
+        FieldPanel('link_external'),
+        PageChooserPanel('link_page'),
+    ]
+
+    class Meta:
+        abstract = True
+
+
+# Related links
+class RelatedLink(LinkFields):
+    title = models.CharField(max_length=255, help_text="Link title")
+
+    panels = [
+        FieldPanel('title'),
+        MultiFieldPanel(LinkFields.panels, "Link"),
+    ]
+
+    class Meta:
+        abstract = True
+
+
+class BlogIndexRelatedLink(Orderable, RelatedLink):
+    page = ParentalKey('BlogIndexPage', related_name='related_links')
+
+
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full")
+        FieldPanel('intro', classname="full"),
+        InlinePanel('related_links', label="Related links"),
     ]
