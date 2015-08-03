ea192be06bc63bc7209029cfdd37645e285ccee6
paul-100 <paul.k.karugu@gmail.com>
2015-08-03 17:24:54 +0300

    gallery adding captions

--- apps/gallery/models.py
+++ apps/gallery/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
 from wagtail.wagtailcore.models import Page
 
 class Gallery(Page):
@@ -11,6 +12,8 @@ class Gallery(Page):
         on_delete=models.SET_NULL,
         related_name='+'
     )
+    image1_caption = models.CharField(max_length=255, blank=True)
+
     image2 = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
@@ -18,6 +21,8 @@ class Gallery(Page):
         on_delete=models.SET_NULL,
         related_name='+'
     )
+    image2_caption = models.CharField(max_length=255, blank=True)
+
     image3 = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
@@ -25,6 +30,8 @@ class Gallery(Page):
         on_delete=models.SET_NULL,
         related_name='+'
     )
+    image3_caption = models.CharField(max_length=255, blank=True)
+
     image4 = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
@@ -32,6 +39,8 @@ class Gallery(Page):
         on_delete=models.SET_NULL,
         related_name='+'
     )
+    image4_caption = models.CharField(max_length=255, blank=True)
+
     image5 = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
@@ -39,6 +48,8 @@ class Gallery(Page):
         on_delete=models.SET_NULL,
         related_name='+'
     )
+    image5_caption = models.CharField(max_length=255, blank=True)
+
     image6 = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
@@ -46,12 +57,19 @@ class Gallery(Page):
         on_delete=models.SET_NULL,
         related_name='+'
     )
+    image6_caption = models.CharField(max_length=255, blank=True)
 
     content_panels = Page.content_panels + [
        ImageChooserPanel('image1'),
+       FieldPanel('image1_caption'),
        ImageChooserPanel('image2'),
+       FieldPanel('image2_caption'),
        ImageChooserPanel('image3'),
+       FieldPanel('image3_caption'),
        ImageChooserPanel('image4'),
+       FieldPanel('image4_caption'),
        ImageChooserPanel('image5'),
+       FieldPanel('image5_caption'),
        ImageChooserPanel('image6'),
+       FieldPanel('image6_caption'),
     ]
