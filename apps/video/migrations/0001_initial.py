3a3c531ccfebff3688dd6b75f637b6b9e94e232c
itsMagondu <MagonduNjenga@gmail.com>
2015-08-03 12:36:42 +0300

    Removed duplicate link

--- apps/video/migrations/0001_initial.py
+++ apps/video/migrations/0001_initial.py
@@ -11,11 +11,8 @@ import modelcluster.contrib.taggit
 class Migration(migrations.Migration):
 
     dependencies = [
-<<<<<<< Updated upstream:apps/video/migrations/0001_initial.py
-=======
         ('taggit', '0001_initial'),
         ('wagtailimages', '0006_add_verbose_names'),
->>>>>>> Stashed changes:apps/blog/migrations/0001_initial.py
         ('wagtailcore', '0001_squashed_0016_change_page_url_path_to_text_field'),
         ('news', '0002_auto_20150730_0952'),
     ]
@@ -57,38 +54,6 @@ class Migration(migrations.Migration):
                 'abstract': False,
             },
             bases=('wagtailcore.page',),
-        ),
-        migrations.CreateModel(
-            name='BlogPageTag',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('content_object', modelcluster.fields.ParentalKey(related_name='tagged_items', to='blog.BlogPage')),
-                ('tag', models.ForeignKey(related_name='blog_blogpagetag_items', to='taggit.Tag')),
-            ],
-            options={
-                'abstract': False,
-            },
-        ),
-        migrations.CreateModel(
-            name='Gallery',
-            fields=[
-                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
-                ('image1', models.ForeignKey(related_name='+', on_delete=django.db.models.deletion.SET_NULL, blank=True, to='wagtailimages.Image', null=True)),
-                ('image2', models.ForeignKey(related_name='+', on_delete=django.db.models.deletion.SET_NULL, blank=True, to='wagtailimages.Image', null=True)),
-                ('image3', models.ForeignKey(related_name='+', on_delete=django.db.models.deletion.SET_NULL, blank=True, to='wagtailimages.Image', null=True)),
-                ('image4', models.ForeignKey(related_name='+', on_delete=django.db.models.deletion.SET_NULL, blank=True, to='wagtailimages.Image', null=True)),
-                ('image5', models.ForeignKey(related_name='+', on_delete=django.db.models.deletion.SET_NULL, blank=True, to='wagtailimages.Image', null=True)),
-                ('image6', models.ForeignKey(related_name='+', on_delete=django.db.models.deletion.SET_NULL, blank=True, to='wagtailimages.Image', null=True)),
-            ],
-            options={
-                'abstract': False,
-            },
-            bases=('wagtailcore.page',),
-        ),
-        migrations.AddField(
-            model_name='blogpage',
-            name='tags',
-            field=modelcluster.contrib.taggit.ClusterTaggableManager(to='taggit.Tag', through='blog.BlogPageTag', blank=True, help_text='A comma-separated list of tags.', verbose_name='Tags'),
         ),
         migrations.AddField(
             model_name='embeddedvideoindexrelatedlink',
