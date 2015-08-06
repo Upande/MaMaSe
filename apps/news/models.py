cef29a51a0c0ca0a1ced35a0d40f95fdcad44305
itsMagondu <MagonduNjenga@gmail.com>
2015-08-06 17:59:41 +0300

    Fixed filtering on news

--- apps/news/models.py
+++ apps/news/models.py
@@ -133,31 +133,40 @@ class NewsIndexPage(Page):
 
         return news_item
 
+    @property
+    def tags(self):
+        #Get all unique tags that belong to the children
+        tags = NewsPage.objects.live().descendant_of(self).order_by('tags__name').distinct('tags__name').values_list('tags__name',flat=True)
+
+        return tags
+        
     def get_context(self, request):
         # Get news_item
         news_item = self.news_item
+        tags = self.tags
 
         # Filter by tag
         tag = request.GET.get('tag')
         category = request.GET.get('category')
 
         if tag:
-            news_items = news.filter(tags__name=tag).live().descendant_of(self)
+            news_item = news_item.filter(tags__name=tag)
 
         if category:
-            news_items = NewsPage.objects.filter(category__name=category).live().descendant_of(self)
-   
+            news_item = news_item.filter(category__name=category)
+            
         # Pagination
         page = request.GET.get('page')
         paginator = Paginator(news_item, 10)  # Show 10 newss per page
         try:
-            news_items = paginator.page(page)
+            news_item = paginator.page(page)
         except PageNotAnInteger:
-            news_items = paginator.page(1)
+            news_item = paginator.page(1)
         except EmptyPage:
-            news_items = paginator.page(paginator.num_pages)
+            news_item = paginator.page(paginator.num_pages)
 
         # Update template context
         context = super(NewsIndexPage, self).get_context(request)
-        context['news'] = news_items
+        context['news'] = news_item
+        context['tags'] = tags
         return context
