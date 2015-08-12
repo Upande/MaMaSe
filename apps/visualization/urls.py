5c9e50702a991ee0e50ba4c87996e24bcf9a2ab4
itsMagondu <MagonduNjenga@gmail.com>
2015-08-12 15:12:28 +0300

    Fixed a few things

--- apps/visualization/urls.py
+++ apps/visualization/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, include, url
 
+from apps.visualization.views import IndexView
 urlpatterns = patterns(
     '',
     url(r'^ts/$', 'apps.visualization.views.logThingspeakData', name='visual-ts'),
-    url(r'^$', 'apps.visualization.views.index', name='visual'),
+    url(r'^$', IndexView.as_view(), name='visual'),
 )
