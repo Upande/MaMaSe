2b4e470ee218cb0909395dd15275f12bd5b18e70
kngeno <kevin@upande.com>
2015-08-21 13:10:14 +0300

    Knowledge platform intro

--- apps/intro/views.py
+++ apps/intro/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render_to_response, HttpResponseRedirect, render
 from django.template.context import RequestContext
 from django.core.urlresolvers import reverse
-from django.views.generic import TemplateView
 
 def index(request):    
-    return render_to_response("gis/gis_platform.html", locals(), context_instance=RequestContext(request))
+    return render_to_response('intro/knowledge_platform.html', locals(), context_instance=RequestContext(request))
