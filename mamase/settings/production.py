7a255ba29a2533e5b521366ab19e4882cf0addf0
itsMagondu <MagonduNjenga@gmail.com>
2015-08-27 10:49:22 +0300

    Changing stuff

--- mamase/settings/production.py
+++ mamase/settings/production.py
@@ -37,4 +37,4 @@ AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-MEDIA_URL = 'http://mamase.s3.amazonaws.com/media/'
+MEDIA_URL = 'https://mamase.s3.amazonaws.com/media/'
