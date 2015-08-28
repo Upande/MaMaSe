90ee7e63fafcd530c2e18910274150321a7db36b
itsMagondu <MagonduNjenga@gmail.com>
2015-08-28 17:41:56 +0300

    Changed order of settings

--- mamase/settings/base.py
+++ mamase/settings/base.py
@@ -187,13 +187,14 @@ ALLOWED_HOSTS = ['*']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
+#For gmail or google apps
+EMAIL_USE_TLS = True
+
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 EMAIL_PORT = 587
 
-#For gmail or google apps
-EMAIL_USE_TLS = True
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 SERVER_EMAIL = EMAIL_HOST_USER
