aa6562989a96c307dffd8efbc2fbb4ab403db29c
itsMagondu <MagonduNjenga@gmail.com>
2015-09-11 13:55:08 +0300

    Fixing conflict between staticfiles dir and static root

--- mamase/settings/base.py
+++ mamase/settings/base.py
@@ -150,12 +150,11 @@ STATICFILES_FINDERS = (
 )
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'assets/static'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
 
 # Wagtail settings
 
