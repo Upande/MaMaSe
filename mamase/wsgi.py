8b272376169d1c1f1c84531f4d6d82797bf9ddf5
itsMagondu <MagonduNjenga@gmail.com>
2015-09-14 12:52:21 +0300

    Removing buggy code

--- mamase/wsgi.py
+++ mamase/wsgi.py
@@ -19,13 +19,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mamase.settings")
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
 
-proxies = {
-"http": os.environ['QUOTAGUARDSTATIC_URL'],
-"https": os.environ['QUOTAGUARDSTATIC_URL']
-}
-
-res = requests.get("http://ip.quotaguard.com/", proxies=proxies)
-print res.text
-
 # Assign QuotaGuard to your environment's http_proxy variable
 os.environ['http_proxy'] = os.environ['QUOTAGUARDSTATIC_URL']
