12a80bbc232ca653bfe228ef743dea329aa16800
itsMagondu <MagonduNjenga@gmail.com>
2015-09-18 20:12:27 +0300

    Added channels to api

--- apps/utils/api.py
+++ apps/utils/api.py
@@ -11,16 +11,20 @@ def getFeeds(request):
     limit = request.GET.get('limit',None)
 
     kwargs = {}
+    args = {}
     
     if channel:
         kwargs[ 'channel_id' ] = channel
+        args['id'] = channel
+        
     if start:
         kwargs[ 'added__gte' ] = start
     if end:
         kwargs[ 'added__lte' ] = end
         
     feed = Feed.objects.filter(**kwargs).values('entry_id','channel_id','added','field1','field2','field3','field4','field5','field6','field7','field8','id')    
-    
+    ch = Channel.objects.filter(**args).values('id','name','description','latitude','longitude','field1','field2','field3','field4','field5','field6','field7','field8')
+
     if limit:
         try:
             limit = int(limit)
@@ -30,8 +34,12 @@ def getFeeds(request):
 
     feed = list(feed)
     feed_without_null = []
+    channel_without_null = []
 
+    for item in ch:
+        channel_without_null.append(dict((k,v) for (k,v) in item.items() if v != ""))
+        
     for item in feed:
         feed_without_null.append(dict((k,v) for (k,v) in item.items() if v is not None))
         
-    return JsonResponse(dict(data=feed_without_null))
+    return JsonResponse(dict(channel=channel_without_null,feed=feed_without_null))
