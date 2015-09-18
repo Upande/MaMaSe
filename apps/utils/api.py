5590ac5965b94f2d85a51c89e7dbe310436b9271
itsMagondu <MagonduNjenga@gmail.com>
2015-09-18 10:10:21 +0300

    Added dynamic filtering on feeds

--- apps/utils/api.py
+++ apps/utils/api.py
@@ -6,11 +6,27 @@ from apps.utils.models import Channel,Feed
 
 def getFeeds(request):
     channel = request.GET.get('channel',None)
+    start = request.GET.get('start',None)
+    end = request.GET.get('end',None)
+    limit = request.GET.get('limit',None)
+
+    kwargs = {}
     
     if channel:
-        feed = Feed.objects.filter(channel_id=channel).values('entry_id','channel_id','added','field1','field2','field3','field4','field5','field6','field7','field8','id')    
-    else:
-        feed = Feed.objects.all().values('entry_id','channel_id','added','field1','field2','field3','field4','field5','field6','field7','field8','id')    
+        kwargs[ 'channel_id' ] = channel
+    if start:
+        kwargs[ 'added__gte' ] = start
+    if end:
+        kwargs[ 'added__lte' ] = end
+        
+    feed = Feed.objects.filter(**kwargs).values('entry_id','channel_id','added','field1','field2','field3','field4','field5','field6','field7','field8','id')    
+    
+    if limit:
+        try:
+            limit = int(limit)
+            feed = feed[:limit]
+        except:
+            pass
 
     feed = list(feed)
     feed_without_null = []
