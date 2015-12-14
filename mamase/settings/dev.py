2db9ee622722fee0f43e5a4d7b4c1df1c8268f50
itsMagondu <MagonduNjenga@gmail.com>
2015-12-14 18:24:11 +0300

    Moved media urls

--- mamase/settings/dev.py
+++ mamase/settings/dev.py
@@ -41,17 +41,17 @@ WAGTAILSEARCH_BACKENDS = {
     }
 }
 
-AWS_QUERYSTRING_AUTH = False
-AWS_PRELOAD_METADATA = False
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
+#AWS_QUERYSTRING_AUTH = False
+#AWS_PRELOAD_METADATA = False
+#AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+#AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+#AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-MEDIAFILES_LOCATION = 'media'
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+#MEDIAFILES_LOCATION = 'media'
 
 #MEDIA_URL = 'https://mamase.s3.amazonaws.com/media/'
-MEDIA_URL = '/media/'
+#MEDIA_URL = '/media/'
 
 #Use Rabbit for production
 BROKER_URL = 'amqp://upande:upandegani@localhost:5672//'
