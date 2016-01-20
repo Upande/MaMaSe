446245710365a060058147840e189e180169e027
Magondu <MagonduNjenga@gmail.com>
2016-01-20 12:32:02 +0300

    Adding visual captcha for #38

--- mamase/settings/dev.py
+++ mamase/settings/dev.py
@@ -25,7 +25,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'mamase', 
         'USER': 'postgres',
-        'PASSWORD': 'postgres',   
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432', 
     }
