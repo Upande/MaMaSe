25e080273ca3032d2314159a8f54cd16ca43b3b2
itsMagondu <magondunjenga@gmail.com>
2017-06-16 07:03:35 -0400

    commiting server changes

---
+++ add_rivers.py
@@ -0,0 +1,25 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mamase.settings.production")
+
+import django
+django.setup()
+
+from apps.utils.models import Channel, River
+from apps.utils.thingspeak import checkIfRiver
+
+
+def fix():
+    channels = Channel.objects.all()
+    print "Found channels"
+
+    for item in channels:
+        river = checkIfRiver(item.name)
+        if river:
+            print river
+            r, created = River.objects.get_or_create(name=river)
+            item.river_id = r.id
+            item.save()
+    print "all done"
+
+if __name__ == "__main__":
+    fix()
